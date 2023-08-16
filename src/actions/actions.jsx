08dbabe90ffa2fd261cb48aebcaff1e23261049d
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-08-16 15:38:30 +0200

    add arrows and shorcuts for players

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -8,6 +8,10 @@ export const generatePlayers = (color) => {
       startX = 600 - offset;
     }
   
+    const playerLabels = ['GL', 'LB', 'SB', 'DB', 'LK', 'DK', 'PV'];
+  
+    const arrowDirection = color === 'blue' ? 1 : -1;
+  
     return Array.from({ length: 7 }, (_, index) => ({
       x: startX,
       y: 80 + index * 50,
@@ -16,65 +20,82 @@ export const generatePlayers = (color) => {
       number: index + 1,
       hasBall: false,
       path: [],
+      rotation: 0,
+      label: playerLabels[index], // Dodajte oznake za sve igrače, bez obzira na boju
+      arrowDirection,
     }));
   };
+  
+  
 
-  export const setSelectedAnnotationIndex = (index) => ({
-  type: 'SET_SELECTED_ANNOTATION_INDEX',
+export const setSelectedAnnotationIndex = (index) => ({
+  type: "SET_SELECTED_ANNOTATION_INDEX",
   payload: index,
 });
-  
-  export const handlePlayerDrag = (players, setPlayers, index, e) => {
-    const newPlayers = [...players];
-    newPlayers[index] = {
-      ...newPlayers[index],
-      x: e.target.x(),
-      y: e.target.y(),
-    };
-    setPlayers(newPlayers);
-  };
-  
-  export const handlePlayerClick = (players, setPlayers, selectedPlayerIndex, setSelectedPlayerIndex, index, e) => {
-    if (e.evt.button === 2) {
-      const updatedPlayers = players.filter((player, i) => i !== index);
-      setPlayers(updatedPlayers);
-      setSelectedPlayerIndex(null);
-    } else {
-      setPlayers(
-        players.map((player, i) =>
-          i === index ? { ...player, hasBall: !player.hasBall } : player
-        )
-      );
-      setSelectedPlayerIndex(index);
-    }
-  };
-  
-  export const handleAnnotationClick = (index) => {
-    setSelectedAnnotationIndex(index);
-  };
-  
-  export const handleStageClick = (e, selectedAnnotationIndex, annotations, setAnnotations) => {
-    if (selectedAnnotationIndex !== null) {
-      const newAnnotations = [...annotations];
-      const annotation = newAnnotations[selectedAnnotationIndex];
-      annotation.points.push(e.evt.layerX, e.evt.layerY);
-      setAnnotations(newAnnotations);
-      setSelectedAnnotationIndex(null);
-    }
-  };
-  
-  export const handleAnnotationContextMenu = (e, index, annotations, setAnnotations) => {
-    e.evt.preventDefault();
-    const updatedAnnotations = annotations.filter((annotation, i) => i !== index);
-    setAnnotations(updatedAnnotations);
-  };
-  
-  export const removeAnnotation = (index, annotations, setAnnotations) => {
-    const updatedAnnotations = annotations.filter((annotation, i) => i !== index);
-    setAnnotations(updatedAnnotations);
-  };
-  
 
-  
- 
-  
+export const handlePlayerDrag = (players, setPlayers, index, e) => {
+  const newPlayers = [...players];
+  newPlayers[index] = {
+    ...newPlayers[index],
+    x: e.target.x(),
+    y: e.target.y(),
+  };
+  setPlayers(newPlayers);
+};
+
+export const handlePlayerClick = (
+  players,
+  setPlayers,
+  selectedPlayerIndex,
+  setSelectedPlayerIndex,
+  index,
+  e
+) => {
+  if (e.evt.button === 2) {
+    const updatedPlayers = players.filter((player, i) => i !== index);
+    setPlayers(updatedPlayers);
+    setSelectedPlayerIndex(null);
+  } else {
+    setPlayers(
+      players.map((player, i) =>
+        i === index ? { ...player, hasBall: !player.hasBall } : player
+      )
+    );
+    setSelectedPlayerIndex(index);
+  }
+};
+
+export const handleAnnotationClick = (index) => {
+  setSelectedAnnotationIndex(index);
+};
+
+export const handleStageClick = (
+  e,
+  selectedAnnotationIndex,
+  annotations,
+  setAnnotations
+) => {
+  if (selectedAnnotationIndex !== null) {
+    const newAnnotations = [...annotations];
+    const annotation = newAnnotations[selectedAnnotationIndex];
+    annotation.points.push(e.evt.layerX, e.evt.layerY);
+    setAnnotations(newAnnotations);
+    setSelectedAnnotationIndex(null);
+  }
+};
+
+export const handleAnnotationContextMenu = (
+  e,
+  index,
+  annotations,
+  setAnnotations
+) => {
+  e.evt.preventDefault();
+  const updatedAnnotations = annotations.filter((annotation, i) => i !== index);
+  setAnnotations(updatedAnnotations);
+};
+
+export const removeAnnotation = (index, annotations, setAnnotations) => {
+  const updatedAnnotations = annotations.filter((annotation, i) => i !== index);
+  setAnnotations(updatedAnnotations);
+};
--- src/components/PlayerComponent.jsx
+++ src/components/PlayerComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Circle, Text } from 'react-konva';
+import { Circle, Text, Arrow } from 'react-konva';
 
 const PlayerComponent = ({
   player,
@@ -7,7 +7,8 @@ const PlayerComponent = ({
   onPlayerDrag,
   onPlayerClick,
   ballRadius,
-  index, // Dodato ovde
+  index,
+  onUpdateArrowRotation,
 }) => {
   const handleDragEnd = (e) => {
     onPlayerDrag(index, e);
@@ -16,6 +17,13 @@ const PlayerComponent = ({
   const handleClick = (e) => {
     onPlayerClick(index, e);
   };
+
+  const handleArrowDragEnd = (e) => {
+    const newRotation = e.target.rotation();
+    onUpdateArrowRotation(index, newRotation);
+  };
+
+  const playerLabels = ['GL', 'LB', 'SB', 'DB', 'LK', 'DK', 'PV'];
 
   return (
     <React.Fragment>
@@ -27,26 +35,39 @@ const PlayerComponent = ({
         numPoints={5}
         innerRadius={20}
         outerRadius={40}
-       
         opacity={0.8}
         draggable
-      
         shadowColor="black"
         shadowBlur={10}
         shadowOpacity={0.6}
-       
         className={`Player ${selectedPlayerIndex === index ? 'Selected' : ''}`}
-       
         onDragEnd={handleDragEnd}
         onClick={handleClick}
       />
-      <Text x={player.x - 7} y={player.y - 10} text={player.number.toString()} fontSize={14} fill="white" />
+      <Text x={player.x - 7} y={player.y - 10} text={player.label} fontSize={14} fill="white" />
+
       {player.hasBall && (
         <Circle x={player.x} y={player.y - player.radius - ballRadius - 2} radius={ballRadius} fill="black" />
       )}
+   <Arrow
+  x={player.x}
+  y={player.y}
+  points={[0, 0, 27 * player.arrowDirection, 0]} // Promenite tačke na osnovu arrowDirection
+  pointerLength={10}
+  pointerWidth={10}
+  fill="black"
+  draggable
+  resizable
+  rotation={player.rotation}
+  onDragEnd={handleArrowDragEnd}
+/>
+
+
     </React.Fragment>
   );
 };
 
 export default PlayerComponent;
+
+
 
